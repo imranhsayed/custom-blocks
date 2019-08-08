2ce657915a75bd57c5be72a5c1e69c4f7e71e017
Imran Sayed <imranhsayed@gmail.com>
2019-08-09 01:13:54 +0530

    Add attributes to the custom block

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 
 registerBlockType( 'gtcb-blocks/custom-block', {
+
+	attributes: {
+		fullName: {
+			type: 'array',
+			source: 'children',
+			selector: 'p'
+		},
+	},
+
 	title: __( 'Custom Block', 'gutenberg-workshop' ),
 	icon: 'megaphone',
 	category: 'common',
 
-	edit: () => ( <div>Hello World</div> ),
+	edit: ( props ) => {
+		let { attributes , setAttributes, className } = props;
+
+		const onChangeFullName = ( event ) => ( setAttributes( { fullName: event.target.value } ) );
+
+		return ( <p onChange={ onChangeFullName }/>{ attributes.fullName }</p> );
+	},
 	save: () => ( <div>Hello World</div> )
 } );
